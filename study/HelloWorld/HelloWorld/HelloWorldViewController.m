826ebfb59f7c9e33dc852ac89f26a5d8db13090e
masonmei <dongxu.m@gmail.com>
2012-07-22 15:57:58 +0800

    initial import hello world iOS project.

---
+++ study/HelloWorld/HelloWorld/HelloWorldViewController.m
@@ -0,0 +1,38 @@
+//
+//  HelloWorldViewController.m
+//  HelloWorld
+//
+//  Created by Mason Mei on 7/22/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "HelloWorldViewController.h"
+
+@interface HelloWorldViewController ()
+
+@end
+
+@implementation HelloWorldViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation != UIInterfaceOrientationPortraitUpsideDown);
+}
+
+-(void)showMessage{
+    UIAlertView *alertView = [[UIAlertView alloc] initWithTitle:@"My First App" message:@"Hello World" delegate:nil cancelButtonTitle:@"OK" otherButtonTitles: nil];
+    [alertView show];
+}
+@end
